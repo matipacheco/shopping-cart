c4b42b74e205a469ee30ed38c2629dbe487d2e63
Mati <matipacheco@gmail.com>
2019-06-28 22:31:33 -0400

    nvbar

--- src/javascript/components/Header.js
+++ src/javascript/components/Header.js
@@ -3,8 +3,14 @@ import React, { Component } from "react";
 class Header extends Component{
   render() {
     return (
-      <div className="fixed-top flex-lg-row-reverse">
-        <i className="fas fa-shopping-cart"></i>
+      <div className="fixed-top">
+        <nav className="navbar navbar-expand-lg bg-light">
+          <ul className="nav navbar-nav">
+            <li>
+              <i className="fas fa-shopping-cart"></i>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
